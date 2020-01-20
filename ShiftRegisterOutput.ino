9732a8ab5b8f62fb0aed50679a2bc18c8c9d4010
Florianisme <florian.m9782@gmail.com>
2020-01-20 18:23:54 +0100

    Adjust pins to official tutorial

--- ShiftRegisterOutput.ino
+++ ShiftRegisterOutput.ino
@@ -1,9 +1,9 @@
 // Pin connected to ST_CP of 74HC595
-#define LATCH_PIN 16
+#define LATCH_PIN 8
 // Pin connected to SH_CP of 74HC595
-#define CLOCK_PIN 14
+#define CLOCK_PIN 12
 // Pin connected to DS of 74HC595
-#define DATA_PIN 12
+#define DATA_PIN 11
 
 void initShiftRegisterOutput() {
   pinMode(LATCH_PIN, OUTPUT);
